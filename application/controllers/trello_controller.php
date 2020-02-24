ad0d16acdb3b309f998f56dc990849d89da79571
ymasilela <ymasilela1992@gmail.com>
2020-02-24 10:29:14 +0200

    Add files via upload

--- application/controllers/trello_controller.php
+++ application/controllers/trello_controller.php
@@ -7,27 +7,40 @@ class trello_controller extends CI_Controller
     {
         parent::__construct();
         $this->load->helper('url');
-        //$this->load->model('ProjectManagement');
+        $this->load->model('ProjectManagement');
         
         $this->load->library('session');
     }
     
     public function index()
     {
-        $this->load->view("index.php");
+      
+       
+        
+        $projectName = "StartTuts";
+        $projectManagement = new ProjectManagement();
+        $statusResult = $projectManagement->getAllStatus();
+        
+        $this->load->view('index.php', array(
+            'statusResult'=>$statusResult,
+            'projectName'=>$projectName,
+            'projectManagement'=>$projectManagement
+        ));
+       
     }
-    public function trello_add_card()
+    public function trello_add_card($status_d)
     {
-        $user = array(
+       
             
-            'card' => trim ( $this->input->post ( 'card' ) ),
-           
-        );
+            $card=  $this->input->post ( 'card' );
+          
+            
+    
         // $this->form_validation->set_rules('first_name','First Name','trim|required|alpha|min_length[3|max_length[30]]');
         
         $data = null;
-        if ($user['card'] ) {
-            $data=$this->ProjectManagement->addcard($user);
+        if ($status_d) {
+            $data=$this->ProjectManagement->addToCard($card,$status_d);
             $message = "success";
             $this->session->set_flashdata('success_msg', 'Added');
             
@@ -42,7 +55,7 @@ class trello_controller extends CI_Controller
         die(json_encode(array('message'=>$message,'user'=>$data)));
         
     }
-    
+  
     //delete card
     function delete_user_card($id)
     {
