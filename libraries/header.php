2a83ced208180827bcfd8cbaa5232a5b9e5b43b8
ymasilela <ymasilela1992@gmail.com>
2020-02-24 09:40:17 +0200

    Add files via upload

---
+++ libraries/header.php
@@ -0,0 +1,39 @@
+<html>
+
+<link rel="stylesheet"
+	href="<?php echo base_url('libraries/css/bootstrap.min.css');?>"
+	type="text/css" />
+<link href="<?php echo base_url('libraries/css/materialize.css');?>"
+	type="text/css" rel="stylesheet" media="screen,projection" />
+<link href="<?php echo base_url('libraries/css/style.css');?>"
+	type="text/css" rel="stylesheet" media="screen,projection" />
+<link href="<?php echo base_url('libraries/js/jquery.validate.js');?>"
+	type="text/js" rel="javascript" media="screen,projection" />
+
+		
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
+    
+<nav class="white lighten-1" role="navigation">
+	<div class="nav-wrapper container">
+		<a href="#"><img src="<?php echo base_url('images/logo.svg'); ?>"
+			width="150px" height="30px" class="img-fluid flex-left"></a>
+		<ul class=" right hide-on-med-and-down">
+			<li><a class="black-text" href="#">PHP Test</a></li>
+			<li><a href="<?php echo base_url('user/user_logout');?>">
+					<button type="button" class="btn btn-info btn-lg ">Logout</button>
+			</a></li>
+		</ul>
+
+	</div>
+</nav>
+
+
+
+	<!--  Scripts-->
+	
+	<script src="<?php echo base_url('libraries/js/materialize.js');?>"></script>
+	<script src="<?php echo base_url('libraries/js/init.js');?>"></script>
+
+
+</html>
